81f1b8d1b7cfd0cd43d4d2e5626f9ffd83ccbbdb
hmdnz <hammadmcferri@gmail.com>
2024-06-13 10:49:52 +0100

    first commit

--- index.js
+++ index.js
@@ -7,16 +7,15 @@ dotenv.config(); // Load environment variables
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 async function run() {
-    // For text-only input, use the gemini-pro model
-    const model = genAI.getGenerativeModel({model:"gemini-pro"});
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-    const prompt="write a story about a magic backpack"
+  const prompt = "write a story about a magic backpack";
 
-    const result =await model.generateContent(prompt)
-    const response= await result.response
-    const text = await response.text()
-    console.log(text)
-
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = await response.text();
+  console.log(text);
 }
 
-run()
+run();
